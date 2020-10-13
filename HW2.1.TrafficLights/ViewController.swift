56ab4e7a448e2151feee895aecc5086450916132
Vladislav <vladislav.y.popov@gmail.com>
2020-10-13 15:27:40 +0300

    Traffic light view completed

--- HW2.1.TrafficLights/ViewController.swift
+++ HW2.1.TrafficLights/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redTrafficLightView: UIView!
+    @IBOutlet weak var yellowTrafficLightView: UIView!
+    @IBOutlet weak var greenTrafficLightView: UIView!
+    
+    @IBOutlet weak var toggleTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redTrafficLightView.layer.cornerRadius = 60
+        yellowTrafficLightView.layer.cornerRadius = 60
+        greenTrafficLightView.layer.cornerRadius = 60
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toggleTextButtonPressed() {
+        redTrafficLightView.alpha = 1
+        toggleTextButton.setTitle("NEXT...", for: .normal)
+    }
+    
+    
 }
 
